359a16f9146cc2390c610c8e40d3c610174f98c1
Benit Polvie <polviedivine@gmail.com>
2026-06-04 15:37:26 +0200

    Update bank.py

--- bank.py
+++ bank.py
@@ -2,12 +2,14 @@ def main():
     user_input = input('Greeting: ')
     print(greeting(user_input))
 
+
 def greeting(greet):
     if greet.lower().startswith('hello'):
         return "$0"
     elif greet.lower().startswith('h'):
         return "$20"
     return "$100"
+
 
 if __name__ == "__main__":
     main()
